1e02b8df71198a50d7d44a4fafab758490dd8859
draknem <draknem@gmail.com>
2021-09-24 01:16:58 +0300

    Update cookie-garden-helper.js

--- cookie-garden-helper.js
+++ cookie-garden-helper.js
@@ -645,10 +645,6 @@ setTimeout(function waitForGame() {
 if (typeof Game === 'object' && Game.ready) {
   if (Garden.isActive) {
     Main.init();
-  } else {
-    let msg = `You don't have a garden yet. This mod won't work without it!`;
-    console.log(msg);
-    UI.createWarning(msg);
   }
 }
 else setTimeout(waitForGame,100)
